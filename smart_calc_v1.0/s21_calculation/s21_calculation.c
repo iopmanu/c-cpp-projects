c52f5a0cb095a499269a571c7ec2a586c0fac4a3
DoroninIvan <ivan.doronin2018@yandex.ru>
2022-12-27 17:42:31 +0300

    main - better calculation decomposition

--- smart_calc_v1.0/s21_calculation/s21_calculation.c
+++ smart_calc_v1.0/s21_calculation/s21_calculation.c
@@ -6,117 +6,13 @@ int8_t calculate(token_t *postfix, int length, double x, double *answer) {
     stk_t double_stk;
     stack_ctor(&double_stk, REAL_NUMBER_DATA);
     int8_t check = false;
-    double first = 0, second = 0;
 
     for (int i = 0; (i < length) && (!check); i++) {
         if (postfix[i].is_number) {
             push(&double_stk, postfix[i].number, POISON_PTR);
         } else {
-            switch (postfix[i].operator) {
-                case 'x':
-                    push(&double_stk, x, POISON_PTR);
-                    break;
-                case '+':
-                    if (valid_calculations_stack(double_stk)) {
-                        push(&double_stk,
-                             pop_number_data(&double_stk) + pop_number_data(&double_stk),
-                             POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case '-':
-                    if (valid_calculations_stack(double_stk)) {
-                        second = pop_number_data(&double_stk);
-                        push(&double_stk, pop_number_data(&double_stk) - second, POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case '*':
-                    if (valid_calculations_stack(double_stk)) {
-                        push(&double_stk,
-                             pop_number_data(&double_stk) * pop_number_data(&double_stk),
-                             POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case '/':
-                    if (valid_calculations_stack(double_stk) &&
-                        (fabs(second = pop_number_data(&double_stk) >= EPS))) {
-                        push(&double_stk, pop_number_data(&double_stk) / second, POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case SIN:
-                    if (double_stk.top != NULL) {
-                        push(&double_stk, sin(pop_number_data(&double_stk)), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case COS:
-                    if (double_stk.top != NULL) {
-                        push(&double_stk, cos(pop_number_data(&double_stk)), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case TAN:
-                    first = pop_number_data(&double_stk);
-                    if (double_stk.top != NULL && fabs(cos(first)) >= 0) {
-                        push(&double_stk, tan(first), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case ASIN:
-                    if (double_stk.top != NULL) {
-                        push(&double_stk, asin(pop_number_data(&double_stk)), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case ACOS:
-                    if (double_stk.top != NULL) {
-                        push(&double_stk, acos(pop_number_data(&double_stk)), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case ATAN:
-                    if (double_stk.top != NULL) {
-                        push(&double_stk, atan(pop_number_data(&double_stk)), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case SQRT:
-                    first = pop_number_data(&double_stk);
-                    if (double_stk.top != NULL && first >= 0) {
-                        push(&double_stk, sqrt(first), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case LOG:
-                    first = pop_number_data(&double_stk);
-                    if (double_stk.top != NULL && first > EPS) {
-                        push(&double_stk, log10(first), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-                case LN:
-                    if (double_stk.top != NULL && first > EPS) {
-                        push(&double_stk, log(first), POISON_PTR);
-                    } else {
-                        check = true;
-                    }
-                    break;
-            }
+            function_calculation(postfix[i].operator, & double_stk, &check);
+            arithmetic_processing(postfix[i].operator, & double_stk, &check, x);
         }
     }
 
@@ -130,6 +26,121 @@ int8_t calculate(token_t *postfix, int length, double x, double *answer) {
     stack_dtor(&double_stk);
 
     return check;
+}
+
+void arithmetic_processing(char operator, stk_t * double_stk, int8_t *check, double x) {
+    double second = 0;
+
+    switch (operator) {
+        case 'x':
+            push(double_stk, x, POISON_PTR);
+            break;
+        case '+':
+            if (valid_calculations_stack(*double_stk)) {
+                push(double_stk, pop_number_data(double_stk) + pop_number_data(double_stk),
+                     POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case '-':
+            if (valid_calculations_stack(*double_stk)) {
+                second = pop_number_data(double_stk);
+                push(double_stk, pop_number_data(double_stk) - second, POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case '*':
+            if (valid_calculations_stack(*double_stk)) {
+                push(double_stk, pop_number_data(double_stk) * pop_number_data(double_stk),
+                     POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case '/':
+            if (valid_calculations_stack(*double_stk) &&
+                (fabs(second = pop_number_data(double_stk) >= EPS))) {
+                push(double_stk, pop_number_data(double_stk) / second, POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+    }
+}
+
+void function_calculation(char function, stk_t *double_stk, int8_t *check) {
+    double first = 0;
+
+    switch (function) {
+        case SIN:
+            if (double_stk->top != NULL) {
+                push(double_stk, sin(pop_number_data(double_stk)), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case COS:
+            if (double_stk->top != NULL) {
+                push(double_stk, cos(pop_number_data(double_stk)), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case TAN:
+            first = pop_number_data(double_stk);
+            if (double_stk->top != NULL && fabs(cos(first)) >= 0) {
+                push(double_stk, tan(first), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case ASIN:
+            if (double_stk->top != NULL) {
+                push(double_stk, asin(pop_number_data(double_stk)), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case ACOS:
+            if (double_stk->top != NULL) {
+                push(double_stk, acos(pop_number_data(double_stk)), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case ATAN:
+            if (double_stk->top != NULL) {
+                push(double_stk, atan(pop_number_data(double_stk)), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case SQRT:
+            first = pop_number_data(double_stk);
+            if (double_stk->top != NULL && first >= 0) {
+                push(double_stk, sqrt(first), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case LOG:
+            first = pop_number_data(double_stk);
+            if (double_stk->top != NULL && first > EPS) {
+                push(double_stk, log10(first), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+        case LN:
+            if (double_stk->top != NULL && first > EPS) {
+                push(double_stk, log(first), POISON_PTR);
+            } else {
+                (*check) = true;
+            }
+            break;
+    }
 }
 
 int8_t valid_calculations_stack(stk_t stk) { return (stk.top != NULL && stk.top->next != NULL); }
--- smart_calc_v1.0/s21_calculation/s21_calculation.h
+++ smart_calc_v1.0/s21_calculation/s21_calculation.h
@@ -8,8 +8,48 @@
 
 #include "../parser/s21_parser.h"
 
+/**
+ * @brief This function to check ability to make arithmetic operation with two numbers in stack.
+ * 
+ * @param stk - source stack of double values
+ * @return int8_t 1 if it's possible, 0 - not
+ */
 int8_t valid_calculations_stack(stk_t stk);
 
+/**
+ * @brief This function is proccessing sequence of tokens(postfix expression). If token is a number
+ *        we push it into stack. If token is arithmetic operator we need two numbers in stack at this
+ *        moment to make calculations => we check this using function -valid_calculations_stack- and
+ *        if it's possible we use numbers with arithmetic operator and push it into stack of doubles.
+ *        If token is a mathematical function we check is it possible to calculate value of function
+ *        for value on the top of the stack and then push it into the stack. At the end of the function
+ *        we can get result of all calculations at the top of the double stack. If stack contains not
+ *        one number => invalid calculations and we return error code.
+ * 
+ * @param postfix - sequence of tokens
+ * @param length - length of the sequence of tokens
+ * @param x - point on Ox axis
+ * @param answer - result of calculations
+ * @return int8_t 1 if here is some errors occured calculations, 0 if success.
+ */
 int8_t calculate(token_t *postfix, int length, double x, double *answer);
+
+/**
+ * @brief This function to proccess operator in the token and calculate function with values in the stack.
+ * 
+ * @param function - code of the mathematical function
+ * @param double_stk - stack of values
+ * @param check - checking errors occured with calculations
+ */
+void function_calculation(char function, stk_t *double_stk, int8_t *check);
+
+/**
+ * @brief This function to proccess arithmetetical operator in the token and calculate result with stack values.
+ * 
+ * @param double_stk - stack of values
+ * @param check - checking errors occured with calculations
+ * @param x - if there is x value in the token we push it into stack
+ */
+void arithmetic_processing(char operator, stk_t *double_stk, int8_t *check, double x);
 
 #endif // SRC_CALCULATION_H_
