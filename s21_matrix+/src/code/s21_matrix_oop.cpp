d7138c560a0a7942fa7df94d37e9d03abccebe2b
DoroninIvan <ivan.doronin2018@yandex.ru>
2023-01-10 01:34:08 +0300

    main - basic arithmetic functions, getters, setters

---
+++ s21_matrix+/src/code/s21_matrix_oop.cpp
@@ -0,0 +1,165 @@
+#include "../include/s21_matrix_oop.h"
+
+LinearAlgebra::Matrix::Matrix() : rows(0), columns(0), matrix(nullptr) {}
+
+LinearAlgebra::Matrix::Matrix(int rows_, int columns_) : rows(rows_), columns(columns_) {
+    if (rows_ < 1 || columns_ < 1) {
+        throw std::invalid_argument("Wrong value of rows/columns");
+    }
+
+    matrix = new double[rows_ * columns_]();
+}
+
+LinearAlgebra::Matrix::Matrix(const Matrix &other) : rows(other.rows), columns(other.columns) {
+    matrix = new double[other.rows * other.columns];
+    std::copy_n(other.matrix, other.rows * other.columns, matrix);
+}
+
+LinearAlgebra::Matrix::Matrix(Matrix &&other) {
+    rows = std::move(other.rows);
+    columns = std::move(other.columns);
+    matrix = std::move(other.matrix);
+}
+
+LinearAlgebra::Matrix::~Matrix() {
+    rows = 0;
+    columns = 0;
+    delete[] matrix;
+}
+
+int constexpr LinearAlgebra::Matrix::GetColumns() const noexcept { return columns; }
+
+int constexpr LinearAlgebra::Matrix::GetRows() const noexcept { return rows; }
+
+double &LinearAlgebra::Matrix::operator()(int row, int col) {
+    if (rows >= row || columns >= col) {
+        throw std::invalid_argument("Wrond value of rows/columns");
+    }
+
+    return matrix[row * columns + col];
+}
+
+const double &LinearAlgebra::Matrix::At(int row, int col) const {
+    if (row >= rows || col >= columns) {
+        throw std::invalid_argument("Wrond value of rows/columns");
+    }
+
+    return matrix[row * columns + col];
+}
+
+void LinearAlgebra::Matrix::SetColumns(const int &columns_) {
+    if (columns_ < 1) {
+        throw std::invalid_argument("Wrong value of columns");
+    }
+
+    Resize();
+}
+
+void LinearAlgebra::Matrix::SetRows(const int &rows_) {
+    if (rows_ < 1) {
+        throw std::invalid_argument("Wrong value of rows");
+    }
+
+    Resize();
+}
+
+void LinearAlgebra::Matrix::Resize() {
+    Matrix reshaped{rows, columns};
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            reshaped(i, j) = At(i, j);
+        }
+    }
+
+    *this = reshaped;
+}
+
+bool LinearAlgebra::Matrix::EqMatrix(const Matrix &other) const {
+    if (other.rows != rows || other.columns != columns) {
+        return false;
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            if (std::fabs(At(i, j) - other.At(i, j)) >= EPS) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void LinearAlgebra::Matrix::SumMatrix(const Matrix &other) {
+    if (other.rows != rows || other.columns != columns) {
+        throw std::logic_error("Rows and columns should be the same");
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            (*this)(i, j) += other.At(i, j);
+        }
+    }
+}
+
+void LinearAlgebra::Matrix::SubMatrix(const Matrix &other) {
+    if (other.rows != rows || other.columns != columns) {
+        throw std::logic_error("Rows and columns should be the same");
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            (*this)(i, j) -= other.At(i, j);
+        }
+    }
+}
+
+void LinearAlgebra::Matrix::MulNumber(const double num) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            (*this)(i, j) *= num;
+        }
+    }
+}
+
+void LinearAlgebra::Matrix::MulMatrix(const Matrix &other) {
+    if (columns != other.rows) {
+        throw std::logic_error("Columns of the left matrix shoud be equal to rows of the right one");
+    }
+
+    Matrix result { columns, other.rows };
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            for (int k = 0; k < columns; k++) {
+                result(i, j) += At(i, j) * other.At(i, j);
+            }
+        }
+    }
+
+    *this = std::move(result);
+}
+
+bool LinearAlgebra::Matrix::operator==(const Matrix &other) noexcept { return EqMatrix(other); }
+
+LinearAlgebra::Matrix &LinearAlgebra::Matrix::operator+=(const Matrix &other) {
+    SumMatrix(other);
+    return *this;
+}
+
+LinearAlgebra::Matrix &LinearAlgebra::Matrix::operator-=(const Matrix &other) {
+    SubMatrix(other);
+    return *this;
+}
+
+LinearAlgebra::Matrix &LinearAlgebra::Matrix::operator*=(const double num) {
+    MulNumber(num);
+    return *this;
+}
+
+LinearAlgebra::Matrix &LinearAlgebra::Matrix::operator*=(const Matrix &other) {
+    MulMatrix(other);
+    return *this;
+}
+
--- s21_matrix+/src/include/s21_matrix_oop.h
+++ s21_matrix+/src/include/s21_matrix_oop.h
@@ -4,33 +4,36 @@
 #define EPS 1e-7
 
 #include <algorithm>
+#include <cmath>
 #include <exception>
+#include <stdexcept>
 
 namespace LinearAlgebra {
 class Matrix {
 private:
-    std::size_t rows, columns;
-    double **matrix;
+    int rows, columns;
+    double *matrix;
 
 public:
     Matrix();
-    Matrix(std::size_t rows, std::size_t columns);
+    Matrix(int rows, int columns);
     Matrix(const Matrix &other);
     Matrix(Matrix &&other);
     ~Matrix();
 
-    constexpr std::size_t GetRows() const noexcept;
-    constexpr std::size_t GetColumns() const noexcept;
-    double &operator()(std::size_t row, std::size_t col);
-    const double &At(std::size_t i, std::size_t j) const;
+    constexpr int GetRows() const noexcept;
+    constexpr int GetColumns() const noexcept;
+    double &operator()(int row, int col);
+    const double &At(int row, int col) const;
 
-    void SetRows(const std::size_t &rows_);
-    void SetColumns(const std::size_t &columns);
+    void SetRows(const int &rows_);
+    void SetColumns(const int &columns);
+    void Resize();
 
     Matrix operator+(const Matrix &other) const;
     Matrix operator-(const Matrix &other) const;
     Matrix operator*(const Matrix &other) const;
-    Matrix operator*(double number) const;
+    Matrix operator*(const double number) const;
 
     bool operator==(const Matrix &other) noexcept;
     Matrix &operator=(const Matrix &other);
@@ -41,10 +44,10 @@ public:
     Matrix &operator*=(double number);
 
     bool EqMatrix(const Matrix &other) const;
-    void SumMatrix(const Matrix &other) const;
-    void SubMatrix(const Matrix &other) const;
-    void MulNumber(double num) const;
-    void MulMatrix(const Matrix &other) const;
+    void SumMatrix(const Matrix &other);
+    void SubMatrix(const Matrix &other);
+    void MulNumber(const double num);
+    void MulMatrix(const Matrix &other);
     Matrix Transpose() const;
     Matrix CalcComplements() const;
     double Determinant() const;
