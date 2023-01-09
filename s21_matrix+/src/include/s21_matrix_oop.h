9cf41859e7dd3fc86767ec27e00a610b7b53c24a
DoroninIvan <ivan.doronin2018@yandex.ru>
2023-01-10 00:08:49 +0300

    main - codestyle target

--- s21_matrix+/src/include/s21_matrix_oop.h
+++ s21_matrix+/src/include/s21_matrix_oop.h
@@ -7,49 +7,49 @@
 #include <exception>
 
 namespace LinearAlgebra {
-    class Matrix {
-        private:
-            std::size_t rows, columns;
-            double **matrix;
+class Matrix {
+private:
+    std::size_t rows, columns;
+    double **matrix;
 
-        public:
-            Matrix();
-            Matrix(std::size_t rows, std::size_t columns);
-            Matrix(const Matrix &other);
-            Matrix(Matrix &&other);
-            ~Matrix();
+public:
+    Matrix();
+    Matrix(std::size_t rows, std::size_t columns);
+    Matrix(const Matrix &other);
+    Matrix(Matrix &&other);
+    ~Matrix();
 
-            constexpr std::size_t GetRows() const noexcept;
-            constexpr std::size_t GetColumns() const noexcept;
-            double &operator()(std::size_t row, std::size_t col);
-            const double &At(std::size_t i, std::size_t j) const;
+    constexpr std::size_t GetRows() const noexcept;
+    constexpr std::size_t GetColumns() const noexcept;
+    double &operator()(std::size_t row, std::size_t col);
+    const double &At(std::size_t i, std::size_t j) const;
 
-            void SetRows(const std::size_t &rows_);
-            void SetColumns(const std::size_t &columns);
+    void SetRows(const std::size_t &rows_);
+    void SetColumns(const std::size_t &columns);
 
-            Matrix operator+(const Matrix &other) const;
-            Matrix operator-(const Matrix &other) const;
-            Matrix operator*(const Matrix &other) const;
-            Matrix operator*(double number)const;
+    Matrix operator+(const Matrix &other) const;
+    Matrix operator-(const Matrix &other) const;
+    Matrix operator*(const Matrix &other) const;
+    Matrix operator*(double number) const;
 
-            bool operator==(const Matrix &other) noexcept;
-            Matrix &operator=(const Matrix &other);
-            Matrix &operator=(Matrix &&other) noexcept;
-            Matrix &operator+=(const Matrix &other);
-            Matrix &operator-=(const Matrix &other);
-            Matrix &operator*=(const Matrix &other);
-            Matrix &operator*=(double number);
+    bool operator==(const Matrix &other) noexcept;
+    Matrix &operator=(const Matrix &other);
+    Matrix &operator=(Matrix &&other) noexcept;
+    Matrix &operator+=(const Matrix &other);
+    Matrix &operator-=(const Matrix &other);
+    Matrix &operator*=(const Matrix &other);
+    Matrix &operator*=(double number);
 
-            bool EqMatrix(const Matrix &other) const;
-            void SumMatrix(const Matrix &other) const;
-            void SubMatrix(const Matrix &other) const;
-            void MulNumber(double num) const;
-            void MulMatrix(const Matrix&other) const;
-            Matrix Transpose() const;
-            Matrix CalcComplements() const;
-            double Determinant() const;
-            Matrix InverseMatrix() const;
-    };
-}
+    bool EqMatrix(const Matrix &other) const;
+    void SumMatrix(const Matrix &other) const;
+    void SubMatrix(const Matrix &other) const;
+    void MulNumber(double num) const;
+    void MulMatrix(const Matrix &other) const;
+    Matrix Transpose() const;
+    Matrix CalcComplements() const;
+    double Determinant() const;
+    Matrix InverseMatrix() const;
+};
+}  // namespace LinearAlgebra
 
-#endif // SRC_MATRIX_H_
+#endif  // SRC_MATRIX_H_
